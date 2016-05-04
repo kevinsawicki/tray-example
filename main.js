9dc8977475d8704b1f07fb89a12e78d744e6b1d0
Kevin Sawicki <kevinsawicki@gmail.com>
2016-05-04 10:06:51 -0700

    Add initial view

---
+++ main.js
@@ -0,0 +1,41 @@
+const {app, BrowserWindow, Tray} = require('electron')
+const path = require('path')
+
+let tray
+let window
+
+app.dock.hide()
+app.on('ready', () => {
+  createTray()
+  createWindow()
+})
+
+const createTray = () => {
+  tray = new Tray(path.join(__dirname, 'assets', 'trayTemplate.png'))
+  tray.setToolTip('Weather')
+  tray.on('click', (event, bounds) => toggleWindow(bounds))
+  tray.on('double-click', (event, bounds) => toggleWindow(bounds))
+}
+
+const createWindow = () => {
+  window = new BrowserWindow({
+    width: 300,
+    height: 450,
+    show: false,
+    frame: false,
+    fullscreenable: false,
+    resizable: false,
+    transparent: true
+  })
+  window.loadURL(`file://${path.join(__dirname, 'index.html')}`)
+  window.on('blur', () => window.hide())
+}
+
+const toggleWindow = (trayIconBounds) => {
+  const windowBounds = window.getBounds()
+  const x = Math.round(trayIconBounds.x + (trayIconBounds.width / 2) - (windowBounds.width / 2))
+  const y = Math.round(trayIconBounds.y + trayIconBounds.height + 4)
+
+  window.setPosition(x, y, false)
+  window.isVisible() ? window.hide() : window.show()
+}
